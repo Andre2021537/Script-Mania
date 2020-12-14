dae4375b62b7d89f921fea90c2b69ef64b7195a1
Andrew <Andre2021537@live.ytech.edu>
2020-12-14 12:36:39 -0500

    New Files

---
+++ Script Mania/javascript/index.js
@@ -0,0 +1,7 @@
+function launch(type) {
+  if (type == "play") {
+    window.location.href = "../html/play.html"
+  } else if (type == "editor") {
+    window.location.href = "../html/editor.html"
+  }
+}
